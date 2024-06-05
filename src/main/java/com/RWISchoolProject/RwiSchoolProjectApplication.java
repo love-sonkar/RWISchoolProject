0101079eaf1596512c52a4046790b68a8155bb54
love-sonkar <loveboss806@gmail.com>
2024-06-05 11:56:25 +0530

    first commit

---
+++ src/main/java/com/RWISchoolProject/RwiSchoolProjectApplication.java
@@ -0,0 +1,13 @@
+package com.RWISchoolProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwiSchoolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RwiSchoolProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RWISchoolProject/RwiSchoolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RWISchoolProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RwiSchoolProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
